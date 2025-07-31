325d9a68e3f8628c34ba037eb1d9727e4d7f4304
root <savvascyp@hotmail.com>
2025-07-31 10:46:21 +0000

    Update footer

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,6 +1,13 @@
 <hr/>
 <footer>
-    &copy; 2025 <a href="http://savvas.me/unpezable">Unpezable Games</a>
+    <div>
+        &copy; 2025 <a href="http://savvas.me/unpezable">Unpezable Games</a>
+    </div>
+
+    <div style="margin-top: 15px">
+        Follow us: 
+        <a href="http://instagram.com/unpezable.games" target="_blank">Instagram</a> | <a href="https://www.threads.com/@unpezable.games" target="_blank">Threads</a>
+    </div>
 </footer>
 
 <style>
